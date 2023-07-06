85059c82486ac0531dec2eff6a7dd25c20021100
Muhammad Ali <alisoomro493@gmail.com>
2023-07-06 17:35:08 +0500

    Added info section

--- app/src/main/java/com/example/bizcard/MainActivity.kt
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -9,16 +9,18 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bizcard.ui.theme.BizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -57,25 +59,66 @@ fun BizCard(){
             elevation = 8.dp,
 
         ) {
-            //Adding profile image
-            Surface(
-                modifier = Modifier
-                    .size(160.dp)
-                    .padding(4.dp),
-                shape = CircleShape,
-                border = BorderStroke(0.5.dp, Color.DarkGray),
-                elevation = 4.dp,
-                color = MaterialTheme.colors.onSurface.copy(alpha = 0.35f)
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.profile),
-                    contentDescription = "Profile image",
-                    modifier = Modifier.size(140.dp),
-                    contentScale = ContentScale.Crop
+                //Adding profile image
+                ProfileImage()
+                Divider(
+                    color = Color.Black,
+                    thickness = 4.dp,
                 )
+                Info()
             }
 
         }
+    }
+}
+
+@Composable
+private fun Info() {
+    Column(
+        modifier = Modifier.padding(4.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.name),
+            style = MaterialTheme.typography.h4,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primaryVariant,
+            modifier = Modifier.padding(4.dp)
+        )
+        Text(
+            text = stringResource(R.string.title),
+            style = MaterialTheme.typography.h5
+        )
+        Text(
+            text = stringResource(R.string.email),
+            style = MaterialTheme.typography.subtitle1
+        )
+    }
+}
+
+@Composable
+private fun ProfileImage(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(160.dp)
+            .padding(4.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.DarkGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.35f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "Profile image",
+            modifier = Modifier.size(140.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
