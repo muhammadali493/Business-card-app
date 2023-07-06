56ddfaca1cad3f694dc6c4f5cee0a453e5e0755a
Muhammad Ali <alisoomro493@gmail.com>
2023-07-06 19:04:31 +0500

    Added Portfolio button and LazyColumn

--- app/src/main/java/com/example/bizcard/MainActivity.kt
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.bizcard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,10 +25,11 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.bizcard.ui.theme.BizCardTheme
 
+private const val tag = "Button"
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -42,6 +48,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BizCard(){
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -71,8 +80,82 @@ fun BizCard(){
                     thickness = 4.dp,
                 )
                 Info()
+                Button(
+                    onClick = {
+                        buttonClickedState.value = !buttonClickedState.value
+                        Log.d(tag,"Button Clicked!!")
+                    }
+                ) {
+                    Text(
+                        text = "Portfolio",
+                        style = MaterialTheme.typography.button
+                    )
+                }
+                if (buttonClickedState.value){
+                    Content()
+                }else{
+                    Box {
+                        
+                    }
+                }
             }
 
+        }
+    }
+}
+@Preview
+@Composable
+fun Content(modifier: Modifier = Modifier){
+    Box(
+        modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(4.dp)
+    ) {
+        Surface(
+            modifier
+                .padding(4.dp)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+            border = BorderStroke(
+                width = 3.dp,
+                color = Color.Black
+            )
+        ) {
+            Portfolio(
+                data = listOf(
+                "Project 1",
+                "Project 2",
+                "Project 3",
+                "Project 4",
+                "Project 5",
+                "Project 6",
+                "Project 7",
+                "Project 5",
+                "Project 5",
+                "Project 5",
+                "Project 5",
+                "Project 5",
+                "Project 5",
+                "Project 5",
+                "Project 5",
+                "Project 5",
+                "Project 5",
+                "Project 5",
+                "Project 5",
+                "Project 5"
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data){item: String ->
+            Text(text = item)
         }
     }
 }
